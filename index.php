4489817c9f1325f0bf2645d5aa49ee0a0b298714
slavkaa <nikoolin@ukr.net>
2014-11-30 18:57:25 +0200

    #2

--- index.php
+++ index.php
@@ -7,29 +7,91 @@
     <div class="container">
         <br/>
         <div class="row">
+            <div class="col-md-8" style="font-size: 20px;">
+                Данний сервіс допоможе вам підібрати найбільш вдалу діаграмму<br/>
+                базуючись на ваших данних <br/>
+                і тому, на чому ви хочете акцентуваи увагу найбільше.
+            </div>
+        </div>
+        <br/>
+        <div class="row">
+            <div class="col-md-8" style="font-style: italic; font-size: 17px;">
+                Одні і тіж діаграмми підходять іноді для декількох варіантів відповідей.<br/>
+                Нехай вас це не бентежить - у діаграммі головне те, який зміст ви хочете донести.<br/>
+                Тож, будь ласка, обирайте найбільш точну для вашого випадку відповідь <br/>
+                - і прямуйте далі, до своєї ідеальної діаграмми.<br/>
+            </div>
+        </div>
+        <br/>
+        <div class="row">
+            <div class="col-md-8" style="font-size: 20px;">
+                Крок перший:
+            </div>
+        </div>
+
+        <br/>
+        <?php $currentStep = 1; require_once('inc/progress.inc') ?>
+
+        <br/>
+        <div class="row">
             <div class="col-md-8">
                 <h1>Дані якого типу головні на вашій діаграммі?</h1>
             </div>
             <div class="col-md-4"></div>
         </div>
+
         <br/>
+
         <div class="row">
             <div class="col-md-8">
-                <h2>Головнити є цифри, тексти є просто підписами колонок.</h2>
+                <h2>1. Головними є цифри, тексти є просто підписами колонок.</h2>
                 Я хочу показати порівнняння двох або декількох величин,
                 співвіднощення між частинами цілого.<br/>
-                <a href="numbers.php"><h3>Numbers</h3></a>
+                <a href="numbers.php"><h3>Переходьте далі тут.</h3></a>
             </div>
             <div class="col-md-4"></div>
         </div>
+        <div class="row">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="img/chart_illustrations/alghoritm.png" style="width: 80px;" />
+                    <img src="img/chart_illustrations/piramid.png" style="width: 80px;" />
+                    <img src="img/chart_illustrations/schema.png" style="width: 80px;" />
+                    <img src="img/chart_illustrations/timeing.png" style="width: 80px;" />
+                </div>
+            </div>
+        </div>
+
+        <br/>
+        <br/>
         <br/>
         <div class="row">
             <div class="col-md-8">
-                <h2>Головними є тексти, цифр немає взагалі.</h2>
-                <a href="text.php"><h3>Text</h3></a>
+                <h2>2. Головними є тексти, цифр немає взагалі.</h2>
+                <a href="text.php"><h3>Переходьте далі тут.</h3></a>
             </div>
             <div class="col-md-4"></div>
         </div>
+        <div class="col-md-12">
+            <img src="img/chart_illustrations/histogramm.png" style="width: 80px;" />
+            <img src="img/chart_illustrations/linear.png" style="width: 80px;" />
+            <img src="img/chart_illustrations/pie.png" style="width: 80px;" />
+            <img src="img/chart_illustrations/summarizing-histogramm.png" style="width: 80px;" />
+            <img src="img/chart_illustrations/voronka.png" style="width: 80px;" />
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
     </div>
 </body>
 </html>
--- text.php
+++ text.php
@@ -6,41 +6,73 @@
 </head>
 <body>
     <div class="container">
+        <?php $currentStep = 2; require_once('inc/progress.inc') ?>
         <br/>
         <div class="row">
             <div class="col-md-8">
-                Вам важливо показати підпорядкованість елементів один одному.
-                <a href="structure.php">Ієрархія (структура)</a>
+                1. Вам важливо показати підпорядкованість елементів один одному.<br/>
+                <a href="structure.php">Ієрархія (структура)(структура)</a>
             </div>
             <div class="col-md-4"></div>
         </div>
-        <br/>
+        <div class="row">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="img/chart_illustrations/piramid.png" style="width: 80px;" />
+                    <img src="img/chart_illustrations/schema.png" style="width: 80px;" />
+                </div>
+            </div>
+        </div>
 
+        <br/>
         <div class="row">
             <div class="col-md-8">
-                Вам просто потрібно показати нщо дані елементи існують.
+                2. Вам просто потрібно показати що дані елементи існують.
                 Перелік
             </div>
             <div class="col-md-4"></div>
         </div>
-        <br/>
+        <div class="row">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="img/chart_illustrations/list.png" style="width: 80px;" />
+                </div>
+            </div>
+        </div>
 
+        <br/>
         <div class="row">
             <div class="col-md-8">
-                Ви хочете акцентувати увагу глядача на тому, що обьекти на сайді є єтапами первного процессу.
+                3. Ви хочете акцентувати увагу глядача на тому, що обьекти на сайді є єтапами первного процессу.
                 Головна думка не ієрархія, а причинно-наслідковий зв’язок між обьектами.
                 <a href="cause-effect.php">Причинно наслідковий зв’зок</a>
             </div>
             <div class="col-md-4"></div>
         </div>
+        <div class="row">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="img/chart_illustrations/alghoritm.png" style="width: 80px;" />
+                    <img src="img/chart_illustrations/timeing.png" style="width: 80px;" />
+                    <img src="img/chart_illustrations/voronka.png" style="width: 80px;" />
+                </div>
+            </div>
+        </div>
         <br/>
 
-        <div class="row">
+        <!--div class="row">
             <div class="col-md-8">
-                <a href="contrasting.php">Протиставлення об’єктів</a>
+                <a href="contrasting.php">4. Протиставлення об’єктів</a>
             </div>
             <div class="col-md-4"></div>
         </div>
+        <div class="row">
+            <div class="row">
+                <div class="col-md-12">
+                    Не можу вигадати просту діаграмму.
+                </div>
+            </div>
+        </div-->
     </div>
 
 <!--
